0654f784ce933251c7443a13c28ab32563867670
Sachin Kumar <sachin.kumar@shreemaruti.com>
2025-08-13 15:33:25 +0530

    execute command - execvp()

--- main.c
+++ main.c
@@ -40,13 +40,16 @@ int main(){
                 continue;
             }
             pid_t pid = fork();  // sys call to create a new process, duplicating the calling process
-            printf("pid: %d\n", pid);
+            // printf("pid: %d\n", pid);
             if(pid < 0){
                 // forking failed
                 perror("fork"); // fun to print sys call errors
             }else if(pid == 0){  // child process
-                printf("This is a child process. About to run %s\n", argv[0]);
-                exit(0); // 0 - success, 1 - failure
+                if (execvp(argv[0], argv) == -1) {
+                    // If execvp returns, an error occurred
+                    perror(argv[0]);
+                    exit(EXIT_FAILURE); // Exit the child with an error status, inbult macro
+                }
             }else{
                 int status;
                 waitpid(pid, &status, 0); // status - child process info, 0 - default beh, exit when child ends
